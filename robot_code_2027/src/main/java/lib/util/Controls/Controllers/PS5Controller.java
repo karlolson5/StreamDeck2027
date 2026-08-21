17c156e2f1a109e0febf43429b2caae6a00e4701
Karl Olson <olsonkarlp@gmail.com>
2026-08-21 14:07:41 -0400

    remove pointless imports

--- robot_code_2027/src/main/java/lib/util/Controls/Controllers/PS5Controller.java
+++ robot_code_2027/src/main/java/lib/util/Controls/Controllers/PS5Controller.java
@@ -1,12 +1,7 @@
 package frc.lib.util.Controls;
-import org.wpilib.command3.button.CommandGamepad;
 import org.wpilib.command3.Scheduler;
 import org.wpilib.command3.Trigger;
-import org.wpilib.command3.Command;
 import org.wpilib.event.EventLoop;
-import org.wpilib.driverstation.GenericHID.RumbleType;
-import org.wpilib.driverstation.POVDirection;
-import org.wpilib.driverstation.Gamepad;
 
 public class PS5Controller extends PS4Controller {
     
